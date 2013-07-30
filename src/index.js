9a8088115f98076a327b26db2a9fa54a09183597
Tankred Hase <mail@tankredhase.de>
2013-07-30 10:07:42 +0200

    cleanup main js files double require and put require-config link in html fiels

--- src/index.js
+++ src/index.js
@@ -1,43 +1,39 @@
-require(['require-config'], function() {
+require(['jquery', 'js/app-controller', 'js/app-config'], function($, controller, app) {
 	'use strict';
 
-	// Start the main app logic.
-	require(['jquery', 'js/app-controller', 'js/app-config'], function($, controller, app) {
-
-		/**
-		 * Load templates and start the application
-		 */
-		$(document).ready(function() {
-			controller.init(function() {
-				controller.start(startApp);
-			});
+	/**
+	 * Load templates and start the application
+	 */
+	$(document).ready(function() {
+		controller.init(function() {
+			controller.start(startApp);
 		});
-
-		function startApp() {
-			// sandboxed ui in iframe
-			var sandbox = document.getElementById('sandboxFrame').contentWindow;
-
-			// set global listener for events from sandbox
-			window.onmessage = function(e) {
-				var cmd = e.data.cmd;
-				var args = e.data.args;
-
-				// handle the workload in the main window
-				controller.execute(cmd, args, function(resArgs) {
-					// send reponse to sandbox
-					sandbox.postMessage({
-						cmd: cmd,
-						args: resArgs
-					}, '*');
-				});
-			};
-
-			// init sandbox ui
-			sandbox.postMessage({
-				cmd: 'init',
-				args: app.util.tpl.templates
-			}, '*');
-		}
-
 	});
+
+	function startApp() {
+		// sandboxed ui in iframe
+		var sandbox = document.getElementById('sandboxFrame').contentWindow;
+
+		// set global listener for events from sandbox
+		window.onmessage = function(e) {
+			var cmd = e.data.cmd;
+			var args = e.data.args;
+
+			// handle the workload in the main window
+			controller.execute(cmd, args, function(resArgs) {
+				// send reponse to sandbox
+				sandbox.postMessage({
+					cmd: cmd,
+					args: resArgs
+				}, '*');
+			});
+		};
+
+		// init sandbox ui
+		sandbox.postMessage({
+			cmd: 'init',
+			args: app.util.tpl.templates
+		}, '*');
+	}
+
 });
