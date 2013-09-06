2dfe0cac0dcac59deb6f48f35098e9c9a4ca539f
Tankred Hase <tankred@whiteout.io>
2013-09-07 00:34:36 +0200

    add default route

--- src/js/app.js
+++ src/js/app.js
@@ -6,15 +6,17 @@ require(['angular', 'js/controller/message-list', 'angularRoute', 'angularTouch'
 
     var app = angular.module('mail', ['ngRoute', 'ngTouch']);
     app.config(function($routeProvider) {
-        $routeProvider
-            .when('/folders/:folder', {
-                templateUrl: 'tpl/message-list-desktop.html',
-                controller: MessageListCtrl
-            })
-            .when('/folders/:folder/messages/:messageId', {
-                templateUrl: 'tpl/read.html',
-                controller: MessageListCtrl
-            });
+        $routeProvider.when('/folders/:folder', {
+            templateUrl: 'tpl/message-list-desktop.html',
+            controller: MessageListCtrl
+        });
+        $routeProvider.when('/folders/:folder/messages/:messageId', {
+            templateUrl: 'tpl/read.html',
+            controller: MessageListCtrl
+        });
+        $routeProvider.otherwise({
+            redirectTo: '/folders/Inbox'
+        });
     });
 
     angular.element().ready(function() {
