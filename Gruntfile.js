7931acc0043baeaf35d039135f90a3ba7fa7fc64
Tankred Hase <tankred@whiteout.io>
2013-09-15 19:54:58 +0200

    test ci

--- Gruntfile.js
+++ Gruntfile.js
@@ -61,7 +61,6 @@ module.exports = function(grunt) {
         mocha: {
             all: {
                 options: {
-                    timeout: 20000,
                     urls: ['http://localhost:<%= connect.test.options.port %>/test/new-unit/index.html'],
                     run: false
                 }
