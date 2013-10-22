0e08a31b21bf93106cd77954c86342d8820a74eb
Tankred Hase <tankred@whiteout.io>
2013-10-22 14:59:36 +0200

    show error on incorrect input

--- src/js/controller/login-existing.js
+++ src/js/controller/login-existing.js
@@ -5,6 +5,7 @@ define(function(require) {
 
     var LoginExistingCtrl = function($scope, $location) {
         $scope.buttonEnabled = true;
+        $scope.incorrect = false;
 
         $scope.confirmPassphrase = function() {
             var passphrase = $scope.passphrase,
@@ -16,6 +17,7 @@ define(function(require) {
 
             // disable button once loggin has started
             $scope.buttonEnabled = false;
+            $scope.incorrect = false;
             unlockCrypto(imapLogin);
 
             function unlockCrypto(callback) {
@@ -31,22 +33,27 @@ define(function(require) {
 
             function imapLogin(err) {
                 if (err) {
-                    $scope.buttonEnabled = true;
-                    console.error(err);
+                    handleError(err);
                     return;
                 }
 
                 // login to imap backend
                 appController._emailDao.imapLogin(function(err) {
                     if (err) {
-                        $scope.buttonEnabled = true;
-                        console.error(err);
+                        handleError(err);
                         return;
                     }
                     onLogin();
                 });
             }
         };
+
+        function handleError(err) {
+            $scope.incorrect = true;
+            $scope.buttonEnabled = true;
+            $scope.$apply();
+            console.error(err);
+        }
 
         function onLogin() {
             $location.path('/desktop');
