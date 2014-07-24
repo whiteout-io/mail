7b11b43dc4a121dcc79de7160940b5da5a563cfb
Tankred Hase <tankred@whiteout.io>
2014-07-24 09:40:00 +0200

    Add build/singing scripts

--- res/build_cca.sh
+++ res/build_cca.sh
@@ -6,6 +6,14 @@ cd ..
 
 DIR=release/cca
 
+# create
 rm -rf $DIR
 mkdir -p $DIR
 cca create $DIR/Whiteout --link-to=dist/manifest.json
+
+#prepare
+cd $DIR/Whiteout
+cca prepare
+
+# cp signing config
+cp ../../../res/ant.properties ./platforms/android/
