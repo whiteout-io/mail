facc9f13da3a40d9bc1c316cae075db736f21140
Tankred Hase <tankred@whiteout.io>
2014-08-13 13:49:17 +0200

    Fix status bar on iOS

--- src/background.js
+++ src/background.js
@@ -1,11 +1,20 @@
 'use strict';
 
-// open chrome app in new window
 chrome.app.runtime.onLaunched.addListener(function() {
-    chrome.app.window.create('index.html', {
-        'bounds': {
-            'width': 1024,
-            'height': 768
+
+    chrome.runtime.getPlatformInfo(function(info) {
+        // don't render statusbar over app UI on iOS
+        if (info.os === 'cordova-ios' && window.StatusBar) {
+            window.StatusBar.overlaysWebView(false);
         }
+
+        // open chrome app in new window
+        chrome.app.window.create('index.html', {
+            'bounds': {
+                'width': 1024,
+                'height': 768
+            }
+        });
     });
+
 });
